337adf458932da4c749755fa287b286444411351
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-06-06 01:53:19 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,17 +1,14 @@
-import * as express from "express";
-import * as http from "http";
+const express = require('express')
 const path = require('path');
 const app = express()
 const port = 3000;
 const server = require('http').Server(app);
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, "/html/index.html"));
-})
-app.post('/', (req, res) => {
-        
-
+    res.sendFile(path.join(__dirname, "./html/index.html"));
 })
 
 
@@ -19,6 +16,4 @@ app.listen(port, function(){
     console.log('listing, on port ' + port);
 
 })
-
-
 
